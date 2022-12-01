817857c8f3359337c8bc20df6ae88345542019b9
tomribbens <tom2022@rbsservices.be>
2022-12-01 13:07:18 +0100

    Solution for 2022 Day 1

---
+++ aoc_tribbe/aoc2022/d01.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+from aocd.models import Puzzle
+
+
+def solve(data):
+    lines = data.splitlines()
+    elves = defaultdict(int)
+    index = 0
+
+    for line in lines:
+        if line == "":
+            index += 1
+            continue
+
+        elves[index] += int(line)
+
+    parta = max(elves.values())
+    all_elves = sorted(elves.values(), reverse=True)
+    partb = sum(all_elves[0:3])
+
+    return str(parta), str(partb)
+
+
+if __name__ == "__main__":
+    p = Puzzle(year=2022, day=1)
+    print("part a: {}\npart b: {}".format(*solve(p.input_data)))
