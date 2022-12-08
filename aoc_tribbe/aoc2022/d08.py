54e45dd620f319248f01fb95426eae69c8b1e237
tomribbens <tom2022@rbsservices.be>
2022-12-08 08:35:38 +0100

    Refactor: changed x,y variables to row, column
    x and y are typically used the other way round, so to be clearer, switched them to row and column

--- aoc_tribbe/aoc2022/d08.py
+++ aoc_tribbe/aoc2022/d08.py
@@ -8,19 +8,19 @@ def solve(data: str) -> tuple[str, str]:
     total_visible = 0
     scenic_view_scores = []
 
-    for x in range(forest.shape[0]):
-        for y in range(forest.shape[1]):
+    for row in range(forest.shape[0]):
+        for column in range(forest.shape[1]):
             total_visible += any(
                 [
-                    all(forest[i][y] < forest[x][y] for i in range(x - 1, -1, -1)),
+                    all(forest[i][column] < forest[row][column] for i in range(row - 1, -1, -1)),
                     all(
-                        forest[i][y] < forest[x][y]
-                        for i in range(x + 1, forest.shape[0])
+                        forest[i][column] < forest[row][column]
+                        for i in range(row + 1, forest.shape[0])
                     ),
-                    all(forest[x][i] < forest[x][y] for i in range(y - 1, -1, -1)),
+                    all(forest[row][i] < forest[row][column] for i in range(column - 1, -1, -1)),
                     all(
-                        forest[x][i] < forest[x][y]
-                        for i in range(y + 1, forest.shape[1])
+                        forest[row][i] < forest[row][column]
+                        for i in range(column + 1, forest.shape[1])
                     ),
                 ]
             )
@@ -28,33 +28,33 @@ def solve(data: str) -> tuple[str, str]:
             left, right, up, down = 0, 0, 0, 0
             blocked = False
             offset = 1
-            while not blocked and x - offset >= 0:
+            while not blocked and row - offset >= 0:
                 left += 1
-                if forest[x - offset][y] >= forest[x][y]:
+                if forest[row - offset][column] >= forest[row][column]:
                     blocked = True
                 offset += 1
 
             blocked = False
             offset = 1
-            while not blocked and x + offset < forest.shape[0]:
+            while not blocked and row + offset < forest.shape[0]:
                 right += 1
-                if forest[x + offset][y] >= forest[x][y]:
+                if forest[row + offset][column] >= forest[row][column]:
                     blocked = True
                 offset += 1
 
             blocked = False
             offset = 1
-            while not blocked and y - offset >= 0:
+            while not blocked and column - offset >= 0:
                 up += 1
-                if forest[x][y - offset] >= forest[x][y]:
+                if forest[row][column - offset] >= forest[row][column]:
                     blocked = True
                 offset += 1
 
             blocked = False
             offset = 1
-            while not blocked and y + offset < forest.shape[1]:
+            while not blocked and column + offset < forest.shape[1]:
                 down += 1
-                if forest[x][y + offset] >= forest[x][y]:
+                if forest[row][column + offset] >= forest[row][column]:
                     blocked = True
                 offset += 1
 
