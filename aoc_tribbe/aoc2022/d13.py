783c0b34fe429f71538d9f7369f69e3acbcd00ab
tomribbens <tom2022@rbsservices.be>
2022-12-13 14:50:10 +0100

    Refactored 2022 Day 13 solution so it doesn't need special classes anymore

--- aoc_tribbe/aoc2022/d13.py
+++ aoc_tribbe/aoc2022/d13.py
@@ -1,41 +1,10 @@
 from aocd.models import Puzzle
-
-from aoc_tribbe.utils.datatypes import Tristate
-
-
-class Packet:
-    def __init__(self, data: list):
-        self.data = data
-
-    def __repr__(self):
-        return str(self.data)
-
-    def __lt__(self, other):
-        ordered = is_ordered(self.data, other.data)
-        if ordered == True:
-            return True
-        elif ordered == False:
-            return False
-
-    def __gt__(self, other):
-        ordered = is_ordered(other.data, self.data)
-        if ordered == True:
-            return False
-        elif ordered == False:
-            return True
-
-    def __eq__(self, other):
-        return self.data == other.data
+from functools import cmp_to_key
 
 
-def is_ordered(left: int | list[int | list], right: int | list[int | list]) -> Tristate:
+def is_ordered(left: int | list[int | list], right: int | list[int | list]) -> int:
     if isinstance(left, int) and isinstance(right, int):
-        if left < right:
-            return Tristate(True)
-        elif left > right:
-            return Tristate(False)
-        else:
-            return Tristate(None)
+        return left - right
 
     if isinstance(left, int):
         left = [left]
@@ -43,32 +12,27 @@ def is_ordered(left: int | list[int | list], right: int | list[int | list]) -> T
         right = [right]
 
     for l, r in zip(left, right):
-        if (ordered := is_ordered(l, r)) in (True, False):
+        if ordered := is_ordered(l, r):
             return ordered
 
-    if len(left) < len(right):
-        return Tristate(True)
-    elif len(left) > len(right):
-        return Tristate(False)
-    else:
-        return Tristate(None)
+    return len(left) - len(right)
 
 
 def solve(data: str) -> tuple[str, str]:
     pairs = data.split("\n\n")
-    all_packets = [Packet([[2]]), Packet([[6]])]
+    all_packets = [[[2]], [[6]]]
     total = 0
-    for idx, pair in enumerate(pairs):
+    for idx, pair in enumerate(pairs, start=1):
         left = eval(pair.splitlines()[0])
         right = eval(pair.splitlines()[1])
-        all_packets.append(Packet(left))
-        all_packets.append(Packet(right))
-        if is_ordered(left, right) == True:
-            total += idx + 1
+        all_packets.append(left)
+        all_packets.append(right)
+        if is_ordered(left, right) < 0:
+            total += idx
 
-    all_packets.sort()
-    first_packet = all_packets.index(Packet([[2]])) + 1
-    last_packet = all_packets.index(Packet([[6]])) + 1
+    all_packets.sort(key=cmp_to_key(is_ordered))
+    first_packet = all_packets.index([[2]]) + 1
+    last_packet = all_packets.index([[6]]) + 1
 
     return str(total), str(first_packet * last_packet)
 
